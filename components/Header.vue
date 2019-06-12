8adc4502783fc6055a46fd5caa00ebae18423c24
Andrew Bloyce <bloyce1990@gmail.com>
2019-06-12 20:32:27 +1000

    fear: mobile menu (#3)

--- components/Header.vue
+++ components/Header.vue
@@ -1,36 +1,39 @@
 <template>
-  <header id="header">
-    <div class="header-wrapper">
-      <div class="title">
-        <NavLink
-          :link="$site.base"
-          class="home-link"
-        >{{ $site.title }}
-        </NavLink>
+  <section id="header-wrapper">
+    <header id="header">
+      <div class="header-wrapper">
+        <div class="title">
+          <NavLink
+            :link="$site.base"
+            class="home-link"
+          >{{ $site.title }}
+          </NavLink>
+        </div>
+        <div class="header-right-wrap">
+          <ul class="nav" v-if="$themeConfig.nav">
+            <li class="nav-item" v-for="item in $themeConfig.nav">
+              <NavLink :link="item.link">{{ item.text }}</NavLink>
+            </li>
+          </ul>
+          <SearchBox/>
+        </div>
       </div>
-      <div class="header-right-wrap">
-        <ul class="nav" v-if="$themeConfig.nav">
-          <li class="nav-item" v-for="item in $themeConfig.nav">
-            <NavLink :link="item.link">{{ item.text }}</NavLink>
-          </li>
-        </ul>
-        <SearchBox/>
-      </div>
-    </div>
-  </header>
+    </header>
+    <MobileHeader />
+  </section>
 </template>
 
 <script>
+  import MobileHeader from './MobileHeader.vue'
   import SearchBox from '@SearchBox'
-  
+
   export default {
-    components: { SearchBox },
+    components: { MobileHeader, SearchBox },
   }
 </script>
 
 <style lang="stylus">
   @import '~@app/style/config'
-  
   #header
     z-index 12
     position fixed
@@ -43,22 +46,22 @@
     margin auto
     box-shadow: 0 5px 20px rgba(0, 0, 0, 0.03), 0 6px 6px rgba(0, 0, 0, 0.05)
     transition: all 1s cubic-bezier(.25, .8, .25, 1)
-    
+
     ol, ul
       list-style none
       margin 0
       padding 0
-    
+
     &:hover
       box-shadow: 0 5px 20px rgba(0, 0, 0, 0.08), 0 6px 6px rgba(0, 0, 0, 0.1);
-  
+
   // border-bottom 5px solid lighten(#3eaf7c, 50%)
-  
+
   .header-wrapper
     display flex
     line-height 40px
     height 40px
-    
+
     .title
       /*flex 0 0 200px*/
       // color #3eaf7c
@@ -75,59 +78,63 @@
         font-weight bold
         font-family PT Serif, Serif
         text-decoration none
-    
+
     .header-right-wrap
       flex 1
       display flex
       justify-content flex-end
-      
+
       .nav
         flex 0 0 auto
         display flex
         margin 0
         align-items end
-        
-        
+
+
         .nav-item
           flex 1
           margin-left 20px
-          
+
           a
             font-family PT Serif, Serif
             font-size 20px
             // color lighten(#3eaf7c, 30%)
             text-decoration none
             transition color .3s
-      
+
       .search-box
         font-family PT Serif, Serif
         margin-left 20px
-        
+
         input
           border-radius 5px
           transition all .5s
           border: 1px solid #cecece
-          
+
           &:hover
-            border: 1px solid $accentColor
+            border 1px solid $accentColor
             box-shadow 0 0 5px $accentColor
-        
+
         .suggestions
           border 1px solid #000
           top: 40px
           right 0
-          
+
           a
             color #000
             text-decoration none
-            
+
             &.focused
               color $accentColor
-  
+
   @media (max-width: $MQMobile)
+
+    #header
+      display none
+
     .header-wrapper
       flex-direction column
-      
+
       .header-right-wrap
         display none
 </style>
---
+++ components/MobileHeader.vue
@@ -0,0 +1,97 @@
+<template>
+  <div id="mobile-header">
+    <div class="mobile-header-bar">
+      <div class="mobile-header-title">
+        <NavLink
+          :link="$site.base"
+          class="mobile-home-link"
+        >{{ $site.title }}
+        </NavLink>
+        <XIcon v-if="isOpen" @click="isOpen = !isOpen" />
+        <MenuIcon v-else @click="isOpen = !isOpen" />
+      </div>
+      <div class="mobile-menu-wrapper" v-bind:class="{ open: isOpen }">
+        <hr class="menu-divider"/>
+        <ul class="mobile-nav" v-if="$themeConfig.nav">
+          <li class="mobile-nav-item" v-for="item in $themeConfig.nav">
+            <NavLink :link="item.link">{{ item.text }}</NavLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import {
+    MenuIcon,
+    XIcon
+  } from 'vue-feather-icons'
+
+  export default {
+
+    components: {
+      MenuIcon,
+      XIcon
+    },
+
+    data() {
+      return {
+        isOpen: false
+      }
+    }
+}
+</script>
+
+<style lang="stylus">
+
+  .mobile-header-bar
+    z-index 12
+    position fixed
+    top 0
+    width 100vw
+    box-sizing border-box
+    background-color #fff
+    margin auto
+    box-shadow 0 5px 20px rgba(0,0,0,0.03), 0 6px 6px rgba(0,0,0,0.05)
+    transition all 1s cubic-bezier(0.25, 0.8, 0.25, 1)
+
+  #mobile-header
+
+    .mobile-header-title
+      display flex
+      align-items center
+      justify-content space-between
+      padding 1.2em
+
+      .mobile-home-link
+        text-decoration none
+        text-transform uppercase
+        font-family PT Serif, Serif
+        color #222
+        font-weight bold
+
+
+  .mobile-nav-item
+    list-style none
+
+    a
+      text-decoration none
+
+  .menu-divider
+    margin 0
+
+  .mobile-menu-wrapper
+    max-height 0
+    overflow hidden
+    transition 0.3s ease
+    background-color #fff
+
+  .mobile-menu-wrapper.open
+    max-height 450px
+    transition 0.3s ease
+
+  @media (min-width: $MQMobile)
+    #mobile-header
+      display none
+</style>
