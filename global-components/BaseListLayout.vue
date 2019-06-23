42118a3f336b14c40a18f0d0c62ca9779338da5b
ULIVZ <472590061@qq.com>
2019-06-23 19:13:19 +0800

    fix: Vue is undefined

--- global-components/BaseListLayout.vue
+++ global-components/BaseListLayout.vue
@@ -5,21 +5,24 @@
         <div class="ui-post-title">
           <NavLink :link="page.path">{{ page.title }}</NavLink>
         </div>
+        
         <div class="ui-post-summary">
           {{ page.frontmatter.summary || page.summary }}
           <!-- <Content :page-key="page.key" slot-key="intro"/>-->
         </div>
+
         <div class="ui-post-author" v-if="page.frontmatter.author">
           <NavigationIcon/>
           <span>{{ page.frontmatter.author }} in {{ page.frontmatter.location }}</span>
         </div>
+
         <div class="ui-post-date" v-if="page.frontmatter.date">
           <ClockIcon/>
           <span>{{ new Date(page.frontmatter.date.trim()).toDateString() }}</span>
         </div>
       </div>
     </div>
-  
+    
     <component v-if="$pagination.length > 1 && paginationComponent" :is="paginationComponent"></component>
   </div>
 </template>
@@ -27,13 +30,13 @@
 <script>
   /* global THEME_BLOG_PAGINATION_COMPONENT */
   
-  import Toc from '@theme/components/Toc.vue'
+  import Vue from 'vue'
   import { NavigationIcon, ClockIcon } from 'vue-feather-icons'
   import { Pagination, SimplePagination } from '@vuepress/plugin-blog/lib/client/components'
-
+  
   export default {
-    components: { Toc, Pagination, NavigationIcon, ClockIcon },
-    // props: ['pagination'],
+    components: { NavigationIcon, ClockIcon },
+
     data() {
       return {
         paginationComponent: null
@@ -46,7 +49,6 @@
     
     computed: {
       pages() {
-        console.log(this.$pagination)
         return this.$pagination.pages
       },
     },
@@ -57,9 +59,11 @@
         if (n === 'Pagination') {
           return Pagination
         }
+
         if (n === 'SimplePagination') {
           return SimplePagination
         }
+
         return Vue.component(n) || Pagination
       }
     }
@@ -75,10 +79,11 @@
     padding-bottom 25px
     margin-bottom 25px
     border-bottom 1px solid #f1f1f1
+    
     &:last-child
       border-bottom 0px
       margin-bottom 0px
-  
+    
     p
       margin 0
   
@@ -122,12 +127,11 @@
     font-size 12px
     color rgba(0, 0, 0, 0.54)
     font-weight 200
-  
+    
     svg
       margin-right 5px
       width 14px
       height 14px
-
 </style>
 
 <style src="prismjs/themes/prism-okaidia.css"></style>
