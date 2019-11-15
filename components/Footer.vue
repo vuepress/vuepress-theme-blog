465950fe192c3f351f3c8ffe219054ae33e200d4
John Schroeder <john@schroedernet.software>
2019-11-15 14:30:44 -0600

    chore: remove extraneous whitespace from Footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -10,7 +10,7 @@
         </li>
       </ul>
     </div>
-    
+
     <div class="footer-right-wrap">
       <ul class="copyright" v-if="copyright">
         <li class="copyright-item" v-for="item in copyright">
@@ -29,7 +29,7 @@
     InstagramIcon,
     LinkedinIcon
   } from 'vue-feather-icons'
-  
+
   export default {
     components: {
       GithubIcon,
@@ -38,7 +38,7 @@
       InstagramIcon,
       LinkedinIcon
     },
-    
+
     methods: {
       getIconComponentName(contactType) {
         switch (contactType) {
@@ -57,7 +57,7 @@
         }
       },
     },
-    
+
     computed: {
       contact() {
         return (
@@ -71,7 +71,7 @@
           })
           .filter(({ iconComponent }) => iconComponent)
       },
-      
+
       copyright() {
         return (
           this.$themeConfig.footer && this.$themeConfig.footer.copyright || []
@@ -86,7 +86,7 @@
     list-style none
     margin 0
     padding 0
-  
+
   .footer
     height 60px
     box-sizing border-box
@@ -95,28 +95,28 @@
     color #FFF
     display flex
     padding 15px 32px
-    
+
     .footer-left-wrap
       line-height 30px
       flex 1
       display flex
-      
+
       .contact
         display flex
-        
+
         .contact-item
           flex 1
           margin-right 10px
-          
+
           a
             font-size 12px
             color rgba(255, 255, 255, 0.45)
             text-decoration none
             transition color .3s
-            
+
             &:hover
               color #FFF
-    
+
     .footer-right-wrap
       flex 1
       display flex
@@ -125,35 +125,35 @@
 
       @media(min-width: $MQNarrow)
         justify-content flex-end
-      
+
       .copyright
         display flex
         justify-content flex-end
-        
+
         .copyright-item
           flex 0 0 auto
           padding 0 10px
           position relative
           line-height 12px
           border-right 1px solid rgba(255, 255, 255, 0.6)
-          
+
           &:last-child
             border-right none
-          
+
           a
             font-size 12px
             color rgba(255, 255, 255, 0.6)
             text-decoration none
             transition color .3s
-            
+
             &:hover
               color rgba(255, 255, 255, 0.9)
-  
+
   @media (max-width: $MQMobile)
     .footer
       height 100px
       flex-direction column
-      
+
       .footer-left-wrap
         align-items center
         justify-content center
