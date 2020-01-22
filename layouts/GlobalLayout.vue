e58d2cfea0517283f87bf421af2ac3d932f3c83e
creanme <creanme@live.com>
2020-01-22 09:18:28 +0800

    fix: text overflow on the mobile

--- layouts/GlobalLayout.vue
+++ layouts/GlobalLayout.vue
@@ -41,11 +41,13 @@ export default {
 </script>
 
 <style lang="stylus">
-.content-wrapper
-  padding 160px 15px 80px 15px
-  min-height calc(100vh - 80px - 60px - 160px)
-  max-width $contentWidth
-  margin 0 auto
+#vuepress-theme-blog__global-layout
+  word-wrap break-word
+  .content-wrapper
+    padding 160px 15px 80px 15px
+    min-height calc(100vh - 80px - 60px - 160px)
+    max-width $contentWidth
+    margin 0 auto
 
 @media (max-width: $MQMobile)
   .content-wrapper
