c0bc7801f51e050a1af0548831992d72f0d1d9a2
Billyyyyy3320 <newsbielt703@gmail.com>
2020-01-25 17:12:48 +0800

    fix: too much padding at top of page

--- layouts/GlobalLayout.vue
+++ layouts/GlobalLayout.vue
@@ -43,14 +43,15 @@ export default {
 <style lang="stylus">
 #vuepress-theme-blog__global-layout
   word-wrap break-word
-  .content-wrapper
-    padding 160px 15px 80px 15px
-    min-height calc(100vh - 80px - 60px - 160px)
-    max-width $contentWidth
-    margin 0 auto
 
-@media (max-width: $MQMobile)
-  .content-wrapper
-    padding 100px 15px 20px 15px
-    min-height calc(100vh - 20px - 60px - 100px)
+.content-wrapper
+  padding 160px 15px 80px 15px
+  min-height calc(100vh - 80px - 60px - 160px)
+  max-width $contentWidth
+  margin 0 auto
+
+  @media (max-width: $MQMobile)
+    &
+      padding 100px 15px 20px 15px
+      min-height calc(100vh - 20px - 60px - 100px)
 </style>
