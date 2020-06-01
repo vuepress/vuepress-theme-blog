20fb0db1fa695a2c9f0bb2e4ccb7e6cf1a03f946
Rémi Emonet <twitwi@users.noreply.github.com>
2020-06-01 18:13:51 +0200

    Clickable tag labels (incl. outside of the text)
    fixes #84
    
    The diff is huge, mainly due to additionnal nesting (causing more indent in the style).

--- components/PostTag.vue
+++ components/PostTag.vue
@@ -1,6 +1,6 @@
 <template>
   <li class="post-tag">
-    <router-link :to="'/tag/' + tag"> {{ tag }} </router-link>
+    <router-link :to="'/tag/' + tag"><span>{{ tag }}</span></router-link>
   </li>
 </template>
 
@@ -18,48 +18,51 @@ export default {
 
 <style scoped lang="stylus">
 .post-tag
-  background-color $postTagBgColor
-  border-radius 3px 0 0 3px
-  height 26px
-  padding 0 20px 0 23px
-  position relative
-  cursor pointer
+    height 26px
 
   &:not(:last-child)
     margin-right 10px
 
   a
-    color $postTagColor
+    background-color $postTagBgColor
+    border-radius 3px 0 0 3px
+    padding 0 20px 0 23px
+    position relative
     text-decoration none
-    transition color 0.2s
+    height 26px
+    display inline-block
 
-  &:before
-    position absolute
-    left 10px
-    top 10px
-    background #fff
-    border-radius 50%
-    box-shadow inset 0 1px rgba(0, 0, 0, 0.25)
-    content ''
-    height 6px
-    width 6px
+    span
+      color $postTagColor
+      transition color 0.2s
 
-  &:after
-    position absolute
-    right 0
-    top 0
-    background $bgColor
-    border-bottom 13px solid transparent
-    border-left 10px solid $postTagBgColor
-    border-top 13px solid transparent
-    content ''
-
-  &:hover
-    background-color $accentColor
+    &:before
+      position absolute
+      left 10px
+      top 10px
+      background #fff
+      border-radius 50%
+      box-shadow inset 0 1px rgba(0, 0, 0, 0.25)
+      content ''
+      height 6px
+      width 6px
 
     &:after
-      border-left-color $accentColor
+      position absolute
+      right 0
+      top 0
+      background $bgColor
+      border-bottom 13px solid transparent
+      border-left 10px solid $postTagBgColor
+      border-top 13px solid transparent
+      content ''
 
-    a
-      color #fff
+    &:hover
+      background-color $accentColor
+
+      &:after
+        border-left-color $accentColor
+
+      span
+        color #fff
 </style>
