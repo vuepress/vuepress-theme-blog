5759c7b41d9ef993cd09b570b9ad09cd77b3c9a0
Swedish-li <one.piece.li.2016@gmail.com>
2020-07-25 16:22:26 +0900

    fix: Vue hydration fails (fix #89)

--- global-components/BaseListLayout.vue
+++ global-components/BaseListLayout.vue
@@ -14,17 +14,19 @@
         <header class="ui-post-title" itemprop="name headline">
           <NavLink :link="page.path">{{ page.title }}</NavLink>
         </header>
-        <!-- eslint-disable vue/no-v-html -->
-        <p
-          v-if="page.excerpt"
-          class="ui-post-summary"
-          itemprop="description"
-          v-html="page.excerpt"
-        />
-        <!-- eslint-enable vue/no-v-html -->
-        <p v-else class="ui-post-summary" itemprop="description">
-          {{ page.frontmatter.summary || page.summary }}
-        </p>
+        <client-only>
+          <!-- eslint-disable vue/no-v-html -->
+          <p
+            v-if="page.excerpt"
+            class="ui-post-summary"
+            itemprop="description"
+            v-html="page.excerpt"
+          />
+          <!-- eslint-enable vue/no-v-html -->
+          <p v-else class="ui-post-summary" itemprop="description">
+            {{ page.frontmatter.summary || page.summary }}
+          </p>
+        </client-only>
 
         <footer>
           <div
@@ -36,9 +38,9 @@
           >
             <NavigationIcon />
             <span itemprop="name">{{ page.frontmatter.author }}</span>
-            <span v-if="page.frontmatter.location" itemprop="address">
-              &nbsp; in {{ page.frontmatter.location }}
-            </span>
+            <span v-if="page.frontmatter.location" itemprop="address"
+              >&nbsp; in {{ page.frontmatter.location }}</span
+            >
           </div>
 
           <div v-if="page.frontmatter.date" class="ui-post-meta ui-post-date">
@@ -47,9 +49,8 @@
               pubdate
               itemprop="datePublished"
               :datetime="page.frontmatter.date"
+              >{{ resolvePostDate(page.frontmatter.date) }}</time
             >
-              {{ resolvePostDate(page.frontmatter.date) }}
-            </time>
           </div>
 
           <div
@@ -62,9 +63,8 @@
               v-for="tag in resolvePostTags(page.frontmatter.tags)"
               :key="tag"
               :to="'/tag/' + tag"
+              >{{ tag }}</router-link
             >
-              {{ tag }}
-            </router-link>
           </div>
         </footer>
       </article>
@@ -136,77 +136,95 @@ export default {
 </script>
 
 <style lang="stylus">
-.common-layout
-  .content-wrapper
-    padding-bottom 80px
+.common-layout {
+  .content-wrapper {
+    padding-bottom: 80px;
+  }
+}
 
-.ui-post
-  padding-bottom 25px
-  margin-bottom 25px
-  border-bottom 1px solid $borderColor
+.ui-post {
+  padding-bottom: 25px;
+  margin-bottom: 25px;
+  border-bottom: 1px solid $borderColor;
 
-  &:last-child
-    border-bottom 0px
-    margin-bottom 0px
+  &:last-child {
+    border-bottom: 0px;
+    margin-bottom: 0px;
+  }
+}
 
-.ui-post-title
-  font-family PT Serif, Serif
-  font-size 28px
-  border-bottom 0
+.ui-post-title {
+  font-family: PT Serif, Serif;
+  font-size: 28px;
+  border-bottom: 0;
 
-  a
-    cursor pointer
-    color $darkTextColor
-    transition all 0.2s
-    text-decoration none
+  a {
+    cursor: pointer;
+    color: $darkTextColor;
+    transition: all 0.2s;
+    text-decoration: none;
 
-    &:hover
-      text-decoration underline
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+}
 
-.ui-post-summary
-  font-size 14px
-  color rgba($darkTextColor, 0.54)
-  font-weight 200
+.ui-post-summary {
+  font-size: 14px;
+  color: rgba($darkTextColor, 0.54);
+  font-weight: 200;
+}
 
-.ui-post-meta
-  display inline-flex
-  align-items center
-  font-size 12px
-  line-height 12px
+.ui-post-meta {
+  display: inline-flex;
+  align-items: center;
+  font-size: 12px;
+  line-height: 12px;
 
-  &:not(:last-child)
-    margin-bottom 3px
-    margin-right 20px
+  &:not(:last-child) {
+    margin-bottom: 3px;
+    margin-right: 20px;
+  }
 
-  svg
-    margin-right 5px
-    width 14px
-    height 14px
+  svg {
+    margin-right: 5px;
+    width: 14px;
+    height: 14px;
+  }
 
-  @media (max-width: $MQMobile)
-    display flex
+  @media (max-width: $MQMobile) {
+    display: flex;
 
-    &:not(:last-child)
-      margin-bottom 10px
+    &:not(:last-child) {
+      margin-bottom: 10px;
+    }
+  }
+}
 
-.ui-post-author
-  color rgba($darkTextColor, 0.84)
-  font-weight 400
+.ui-post-author {
+  color: rgba($darkTextColor, 0.84);
+  font-weight: 400;
+}
 
-.ui-post-date
-  color rgba($darkTextColor, 0.54)
-  font-weight 200
+.ui-post-date {
+  color: rgba($darkTextColor, 0.54);
+  font-weight: 200;
+}
 
-.ui-post-tag
-  color rgba($darkTextColor, 0.54)
-  font-weight 200
+.ui-post-tag {
+  color: rgba($darkTextColor, 0.54);
+  font-weight: 200;
 
-  a
-    color inherit
-    font-weight 200
-    text-decoration none
-    margin-right 5px
+  a {
+    color: inherit;
+    font-weight: 200;
+    text-decoration: none;
+    margin-right: 5px;
 
-    &:hover
-      color $accentColor
+    &:hover {
+      color: $accentColor;
+    }
+  }
+}
 </style>
