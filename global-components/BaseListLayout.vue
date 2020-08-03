85b520c85e50e5b246ff37eff7bd5ce85ca300a2
Swedish-li <1993770815@qq.com>
2020-08-03 20:07:46 +0900

    fix: ssr hydration error on excerpt summary (fix #89) (#94)

--- global-components/BaseListLayout.vue
+++ global-components/BaseListLayout.vue
@@ -14,14 +14,16 @@
         <header class="ui-post-title" itemprop="name headline">
           <NavLink :link="page.path">{{ page.title }}</NavLink>
         </header>
-        <!-- eslint-disable vue/no-v-html -->
-        <p
-          v-if="page.excerpt"
-          class="ui-post-summary"
-          itemprop="description"
-          v-html="page.excerpt"
-        />
-        <!-- eslint-enable vue/no-v-html -->
+
+        <client-only v-if="page.excerpt">
+          <!-- eslint-disable vue/no-v-html -->
+          <p
+            class="ui-post-summary"
+            itemprop="description"
+            v-html="page.excerpt"
+          />
+          <!-- eslint-enable vue/no-v-html -->
+        </client-only>
         <p v-else class="ui-post-summary" itemprop="description">
           {{ page.frontmatter.summary || page.summary }}
         </p>
